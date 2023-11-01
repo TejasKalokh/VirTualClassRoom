6b9836640bafa10f99f1ca6de5409b4da8e407a4
Tejas Kalokhe <kalokhetejas3@gmail.com>
2023-11-01 18:37:18 +0530

    connected to mysql database

--- ServerSide/Server1.js
+++ ServerSide/Server1.js
@@ -3,6 +3,36 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const port = 3000;
+const mysql = require('mysql2/promise'); // Use 'mysql2/promise' for async/await support
+
+
+// Define your MySQL database connection configuration
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root123',
+  database: 'classroom'
+};
+
+// Create a connection pool
+const pool = mysql.createPool(dbConfig);
+
+// Middleware to attach the MySQL pool to the request object
+app.use((req, res, next) => {
+  req.pool = pool;
+  next();
+});
+
+app.get('/example', async (req, res) => {
+  try {
+    const [rows, fields] = await req.pool.query('SELECT * FROM student');
+    res.json(rows);
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
 
 // app.use(express.static(path.join(__dirname, 'Views')));
 // app.use(express.static(path.join(__dirname, 'Views/style.css')));
--- ServerSide/database.js
+++ ServerSide/database.js
@@ -1,26 +1,36 @@
-// const {
-//     createPool
-// } = require('mysql');
+const express = require('express');
+const mysql = require('mysql2/promise'); // Use 'mysql2/promise' for async/await support
 
-// const pool = createPool({
-//     host: "localhost",
-//     user: 'root',
-//     password: "",
-//     database: "test",
-//     connectionLimit : 10
-// })
+const app = express();
 
-// pool.query(`selet * from registration`, (err, result, fields) => {
-//     if (err) {
-//         return console.log(err);
-//     }
-//     return console.log(result);
-// })
-const mysql = require('mysql');
+// Define your MySQL database connection configuration
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root123',
+  database: 'classroom'
+};
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    database: '',
-    user: 'root',
-    password: 'root'
+// Create a connection pool
+const pool = mysql.createPool(dbConfig);
+
+// Middleware to attach the MySQL pool to the request object
+app.use((req, res, next) => {
+  req.pool = pool;
+  next();
+});
+
+app.get('/example', async (req, res) => {
+  try {
+    const [rows, fields] = await req.pool.query('SELECT * FROM student');
+    res.json(rows);
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
