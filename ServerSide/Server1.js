7896ec43c87e71f83c0fb92f67d3dfc90b7800c7
TejasKalokh <kalokhetejas3@gmail.com>
2023-11-18 22:04:23 +0530

    Firts commit

--- ServerSide/Server1.js
+++ ServerSide/Server1.js
@@ -6,8 +6,11 @@ const fs = require('fs');
 const port = 3000;
 const bodyParser = require('body-parser');
 const { register } = require('module');
+const { name } = require('ejs');
 
-
+// Replace the body-parser middleware with Express's built-in middlewares
+app.use(express.json()); // To parse JSON data
+app.use(express.urlencoded({ extended: true })); // To parse URL-encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('Views'));
@@ -17,9 +20,6 @@ app.set("view engine","ejs");
 
 
 app.get("/", (req,res)=>{
-    res.render("Home");
-})
-app.get("/Home", (req,res)=>{
     res.render("Home");
 })
 
@@ -36,16 +36,54 @@ app.get("/login",(req,res)=>{
 app.get("/Courses",(req,res)=>{
     res.render("Courses");
 })
-app.post('/login', (req, res) => {
+app.post('/login',bodyParser, (req, res) => {
   var namee = req.body.name;
   var idd = req.body.id;
   var passwordd = req.body.password;
-  var sql = "INSERT INTO student(moodle_id,name,password) VALUES('" + idd + "','" + namee + "','" + passwordd + "')";
+    var sql = "INSERT INTO student(moodle_id,name,password) VALUES('" + idd + "','" + namee + "','" + passwordd + "')";
+    var name = namee;
   db.query(sql,function (err, result) {
       res.render('register');
   });
 });
 
+app.get('/login', bodyParser, (req, res) => {
+    var moodle_id = req.body.id;
+    var password = req.body.password;
+    if (moodle_id && password) {
+        query = `Select * From Student(moodle_id,password) Where moodle-id = "${moodle_id}"`;
+        db.query(query, (error, data) => {
+            if (data.length > 0) {
+                for (var count = 0; count < data.length; count++){
+                    if (data[count].password = password) {
+                        req.session.moodle_id = data[count].moodle_id;
+                        res.redirect("/");
+        }
+                    else {
+                        res.send('Incorrect password')
+                    }
+                }
+            }
+            else {
+                res.send('Incorrect MoodelId')
+            }
+        });
+    }
+    else {
+        res.send("Please enter Moodleid and password");
+        res.end();
+    }
+    
+})
+app.get("/home/:username", (req, res) => {
+    var username = name;
+    res.render("home", { username });
+
+});
+app.get("/Home", (req,res)=>{
+    var username = name;
+    res.render("home", { username });
+})
 app.listen(port,()=>{
   console.log(`listening on port ${port}`)
 });
